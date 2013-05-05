576a5a76dfff1e00baeb81d1d9e27e475678b53e
Andy Nagels <thereisanewway@gmail.com>
2013-05-05 12:21:24 +0200

    Changes to the xmonad configs.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,46 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys, additionalKeysP)
+import System.IO
+import XMonad.Actions.CycleWS
+
+terminal' = "urxvtc"
+
+font' = "xft:inconsolata:pixelsize=18:antialias=true:hinting=true"
+
+xmobar' = "/usr/bin/xmobar /home/rockwolf/.xmobarrc -f " ++ font'
+
+main = do
+    xmproc <- spawnPipe xmobar'
+    xmonad $ defaultConfig
+        {
+            -- status bar and dock
+            manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+            , layoutHook = avoidStruts $ layoutHook defaultConfig
+            , logHook = dynamicLogWithPP xmobarPP
+                {
+                    ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "#93a1a1" "" . shorten 50 
+                }
+            -- apps
+            , terminal = terminal'
+            -- key bindings
+            , modMask = mod4Mask
+            -- layout
+            , normalBorderColor = "#002b36"
+            , focusedBorderColor = "#839496"
+        } `additionalKeys`
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ] `additionalKeysP`
+        [ ("M-s-<Tab>", moveTo Prev NonEmptyWS)
+        , ("M-<Tab>", moveTo Next NonEmptyWS)
+        ]
+
+myManageHook = composeAll
+    [
+        className =? "Gimp" --> doFloat
+        , className =? "MPlayer" --> doFloat
+    ]
