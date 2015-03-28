3df9ddb77fc3011cd7ec925bd83783a884d60900
rockwolf <thereisanewway@gmail.com>
2015-03-28 19:40:28 +0100

    Switched to vundle for vim management.

---
+++ freebsd/fish/config.fish
@@ -0,0 +1,4 @@
+do_exports
+
+alias less $PAGER
+alias zless $PAGER
--- freebsd/fish/functions/do_exports.fish
+++ freebsd/fish/functions/do_exports.fish
@@ -1,3 +1,4 @@
 function do_exports --description 'Exports at fish startup'
     set -x PATH /usr/local/sh $PATH
+    set -x PAGER /usr/local/bin/vimpager 
 end
