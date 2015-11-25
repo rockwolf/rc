ca8ce5e6075b11d1a5c783dbc75518b2a0c2f1bf
rockwolf <thereisanewway@gmail.com>
2015-11-25 16:01:47 +0100

    vi keybindings.

--- freebsd/fish/config.fish
+++ freebsd/fish/config.fish
@@ -1,7 +1,10 @@
 # Exports
 set -x PATH /usr/local/sh $PATH
-set -x PAGER /usr/local/bin/vimpager 
+set -x PAGER /usr/local/bin/vimpager
 
 # Aliases
 alias less $PAGER
 alias zless $PAGER
+
+# General settings
+fish_vi_key_bindings
