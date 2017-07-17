04d95cd9f641ffc72595ce5b8bc078622514ab24
rockwolf <thereisanewway@gmail.com>
2017-07-17 23:29:58 +0200

    * vimpager back to less * no colors for ls * aliases added

--- freebsd/fish/config.fish
+++ freebsd/fish/config.fish
@@ -2,16 +2,19 @@
 set -x PATH /usr/local/sh $PATH
 set -x PATH /home/rockwolf/src/bin $PATH
 set -x PATH /home/rockwolf/sh_ $PATH
-set -x PAGER /usr/local/bin/vimpager
+set -x PAGER /usr/bin/less
 set -x APP_ENV development
 set -x EDITOR /usr/local/bin/nvim
+set -x LSCOLORS xxxxxxxxxxx # Use default fg color for everything. This should disable the colors.
 
 # Aliases
 alias less $PAGER
 alias zless $PAGER
 alias t todo.sh
+alias ls "ls -F"
 alias branches "tig --all --simplify-by-decoration"
 alias binclock "tbclock -f -daeHHH -H 0 -M 0 -S 0 -T 0"
+alias timidity "timidity -c /usr/local/share/timidity/timidity.cfg-eawplus"
 
 # General settings
 fish_user_key_bindings
