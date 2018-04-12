01afcbb739de2b2a445fb90eaffde4a95a86e5b8
rockwolf <thereisanewway@gmail.com>
2018-04-12 22:46:09 +0200

    Better layout for ledger-mode.

--- freebsd/emacs/init.el
+++ freebsd/emacs/init.el
@@ -286,13 +286,15 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ledger-font-other-face ((t (:foreground "#eee8d5" :weight normal))))
+ '(ledger-font-payee-cleared-face ((t (:inherit ledger-font-posting-account-face))))
  '(ledger-font-payee-pending-face ((t (:foreground "#2aa198" :weight normal))))
- '(ledger-font-payee-uncleared-face ((t (:foreground "#eee8d5" :weight normal))))
+ '(ledger-font-payee-uncleared-face ((t (:foreground "#899900" :weight normal))))
  '(ledger-font-pending-face ((t (:foreground "#2aa198" :weight normal))))
  '(ledger-font-periodic-xact-face ((t (:foreground "#2aa198" :weight normal))))
- '(ledger-font-posting-account-face ((t (:foreground "#eee8d5"))))
+ '(ledger-font-posting-account-cleared-face ((t (:inherit ledger-font-posting-account-face))))
+ '(ledger-font-posting-account-face ((t (:foreground "#859900"))))
  '(ledger-font-posting-amount-cleared-face ((t (:inherit ledger-font-posting-amount-face))))
- '(ledger-font-posting-amount-face ((t (:foreground "#2aa198"))))
+ '(ledger-font-posting-amount-face ((t (:foreground "#eee8d5"))))
  '(ledger-font-posting-amount-pending-face ((t (:inherit ledger-font-posting-amount-face))))
  '(ledger-font-posting-date-face ((t (:foreground "#93a1a1"))))
  '(ledger-font-xact-pending-face ((t (:foreground "#93a1a1" :weight normal)))))
