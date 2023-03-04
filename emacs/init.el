0bbe7e9d70a770f7eb6979f1077a29a7b1d3b96b
Andy Nagels <thereisanewway@gmail.com>
2023-03-04 21:32:16 +0100

    Uncomment melpa-stable.

--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,7 @@
        (proto (if no-ssl "http" "https")))
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
