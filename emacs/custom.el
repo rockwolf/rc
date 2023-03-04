9127e94a3148b56ccc3703acb933d82189af86b1
Andy Nagels <thereisanewway@gmail.com>
2023-03-04 21:32:37 +0100

    Fixed the config.org.

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,14 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
+   '("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default))
+ '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)
  '(inhibit-startup-screen t)
  '(ledger-init-file-name "~/dev/rc/ledgerrc")
  '(package-selected-packages
-   (quote
-    (powerline-evil rainbow-delimiters diminish evil rust-mode writeroom-mode evil-vimish-fold slime dokuwiki-mode dokuwiki org-link-minor-mode flycheck-ledger ledger-mode paredit ranger neotree ## winum window-number evil-nerd-commenter helm evil-tabs powerline helm-projectile flycheck evil-search-highlight-persist evil-leader color-theme-solarized))))
+   '(org-roam gruvbox-theme evil-collection magit ssh-agency browse-url-dwim powerline-evil rainbow-delimiters diminish evil rust-mode writeroom-mode evil-vimish-fold slime dokuwiki-mode dokuwiki org-link-minor-mode flycheck-ledger ledger-mode paredit ranger neotree ## winum window-number evil-nerd-commenter helm evil-tabs powerline helm-projectile flycheck evil-search-highlight-persist evil-leader color-theme-solarized))
+ '(warning-suppress-types '((initialization) (initialization) (initialization))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
