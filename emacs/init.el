8db51fbaafe9593aba34aed88170d55bfaa4b8da
rockwolf <thereisanewway@gmail.com>
2023-03-07 22:17:00 +0100

    Changes for evy.

--- emacs/init.el
+++ emacs/init.el
@@ -9,13 +9,13 @@
   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  ;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (when (< emacs-major-version 24)
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
-     (message "Loaded packages in %.3fs" elapsed))
+  (message "Loaded packages in %.3fs" elapsed))
 
 (defun require-package (package)
   (setq-default highlight-tabs t)
@@ -28,7 +28,7 @@
 ;; Keep customize settings in a seperate file
 (setq custom-file "~/.emacs.d/custom.el")
 (when (file-exists-p custom-file)
-      (load custom-file))
+  (load custom-file))
 
 (require 'org)
 (org-babel-load-file
@@ -36,4 +36,4 @@
 
 ;; Message how long it took to load everything (minus packages)
 (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
-     (message "Loading settings...done (%.3fs)" elapsed))
+  (message "Loading settings...done (%.3fs)" elapsed))
