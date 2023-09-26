470d59c6983aa6f1ef131eee48a759ffaac348dc
Andy Nagels <thereisanewway@gmail.com>
2023-09-26 21:11:28 +0200

    Remap Neotree toggle to Alt+T

--- nvim/init.lua
+++ nvim/init.lua
@@ -262,7 +262,7 @@ vim.o.termguicolors = true
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<Leader>t', '<cmd>Neotree toggle<cr>')
+vim.keymap.set({'n', 'v'}, '<A-t>', '<cmd>Neotree toggle<cr>')
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
