b249c7aa237cbbb034ee8c0ee5ebd45e96d37210
rockwolf <thereisanewway@gmail.com>
2023-10-20 12:05:49 +0200

    Fixes: - indent-blankline has a breaking change - statusline theme was wrong, changed to gruvbox

--- nvim/init.lua
+++ nvim/init.lua
@@ -120,7 +120,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'gruvbox',
         component_separators = '|',
         section_separators = '',
       },
@@ -131,11 +131,9 @@ require('lazy').setup({
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    -- See `:help ibl`
+    main = "ibl",
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
