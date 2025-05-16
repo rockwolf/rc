d2ca5dca81f06c2288577d9b6dd1cf650caa2d82
Andy Nagels <thereisanewway@gmail.com>
2025-05-16 12:21:17 +0200

    Updates to the fish config.

--- fish/config.fish
+++ fish/config.fish
@@ -10,9 +10,6 @@ set -x EDITOR /usr/local/bin/nvim
 set -x LSCOLORS xxxxxxxxxxx # Use default fg color for everything. This should disable the colors.
 
 # Aliases
-alias less $PAGER
-alias zless $PAGER
-alias t todo.sh
 alias ls "ls -F"
 alias mv "mv -v"
 alias cp "cp -v"
@@ -24,11 +21,18 @@ alias timidity "timidity -c /usr/local/share/timidity/timidity.cfg-eawplus"
 alias sbcl "rlwrap sbcl" # sbcl has no readline history support, but with rlwrap we can simulate it.
 alias gprolog "rlwrap gprolog" # gprolog has no readline history support, but with rlwrap we can simulate it.
 alias swipl "rlwrap swipl" # swipl has no readline history support, but with rlwrap we can simulate it.
-alias vim "emacsclient -c"
-alias vscode "code-oss"
+alias vim "nvim"
 alias gis "git status"
-alias gri "git fetch --all && git rebase -i --rebase-merges"
+alias gri "git fetch --prune --all && git rebase --interactive --rebase-merges"
 alias grc "git rebase --continue"
+alias gim "git pull && git merge"
+alias gbrf "git fetch --prune origin && git branch -rv | grep origin/feature | tail -n 3"
+alias gbrf "git fetch --prune origin && git branch -rv | grep origin/issues | tail -n 10"
+alias plantuml "plantuml -charset UTF-8"
+alias qalc 'qalc -i --terse -s "rpn 1" -s "calculate_as_you_type 1"'
 
 # General settings
 fish_user_key_bindings
+
+# opam configuration
+source /usr/home/rockwolf/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
